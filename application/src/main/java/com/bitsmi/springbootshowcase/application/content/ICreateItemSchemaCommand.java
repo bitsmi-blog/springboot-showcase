e2f10547346da9e9cafedee0d1fef82234b5e602
aarchilla <aarchilla84@gmail.com>
2024-02-25 09:56:09 +0100

    Fixes

--- application/src/main/java/com/bitsmi/springbootshowcase/application/content/ICreateItemSchemaCommand.java
+++ application/src/main/java/com/bitsmi/springbootshowcase/application/content/ICreateItemSchemaCommand.java
@@ -1,8 +1,9 @@
 package com.bitsmi.springbootshowcase.application.content;
 
 import com.bitsmi.springbootshowcase.domain.content.model.ItemSchema;
+import jakarta.validation.Valid;
 
 public interface ICreateItemSchemaCommand
 {
-    ItemSchema createItemSchema(ItemSchema itemSchema);
+    ItemSchema createItemSchema(@Valid ItemSchema itemSchema);
 }
--- core/src/main/java/com/bitsmi/springbootshowcase/core/config/DomainInputPortsConfig.java
+++ core/src/main/java/com/bitsmi/springbootshowcase/core/config/DomainInputPortsConfig.java
@@ -3,7 +3,9 @@ package com.bitsmi.springbootshowcase.core.config;
 import com.bitsmi.springbootshowcase.domain.common.IUserCommandDomainService;
 import com.bitsmi.springbootshowcase.domain.common.impl.UserCommandDomainServiceImpl;
 import com.bitsmi.springbootshowcase.domain.common.spi.IUserPersistenceService;
+import com.bitsmi.springbootshowcase.domain.content.IItemSchemaCommandDomainService;
 import com.bitsmi.springbootshowcase.domain.content.IItemSchemaQueryDomainService;
+import com.bitsmi.springbootshowcase.domain.content.impl.ItemSchemaCommandDomainServiceImpl;
 import com.bitsmi.springbootshowcase.domain.content.impl.ItemSchemaQueryDomainServiceImpl;
 import com.bitsmi.springbootshowcase.domain.content.spi.IItemSchemaPersistenceService;
 import org.springframework.context.annotation.Bean;
@@ -14,14 +16,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class DomainInputPortsConfig
 {
     @Bean
-    public IUserCommandDomainService userCreationCommand(IUserPersistenceService userPersistenceService, PasswordEncoder passwordEncoder)
+    public IUserCommandDomainService userCommandDomainService(IUserPersistenceService userPersistenceService, PasswordEncoder passwordEncoder)
     {
         return new UserCommandDomainServiceImpl(userPersistenceService, passwordEncoder);
     }
 
     @Bean
-    public IItemSchemaQueryDomainService retrieveItemSchemaQuery(IItemSchemaPersistenceService itemSchemaPersistenceService)
+    public IItemSchemaQueryDomainService itemSchemaQueryDomainService(IItemSchemaPersistenceService itemSchemaPersistenceService)
     {
         return new ItemSchemaQueryDomainServiceImpl(itemSchemaPersistenceService);
+    }
+
+    @Bean
+    public IItemSchemaCommandDomainService itemSchemaCommandDomainService(IItemSchemaPersistenceService itemSchemaPersistenceService)
+    {
+        return new ItemSchemaCommandDomainServiceImpl(itemSchemaPersistenceService);
     }
 }
